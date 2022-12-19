c908c18a750ce6c4ade755dd8cd9ae55b69eef39
kevinleonel.abundis.contractor <kevinleonel.abundis.contractor@bbva.com>
2022-12-18 18:06:48 -0600

    Probando nuevas dependencias mock

---
+++ src/test/java/org/abundis/appmockito/ejemplos/services/Datos.java
@@ -0,0 +1,18 @@
+package org.abundis.appmockito.ejemplos.services;
+
+import org.abundis.appmockito.ejemplos.models.Examen;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Datos {
+    public final static List<Examen> EXAMENES = Arrays.asList(new Examen(5L,"Matemáticas"),
+            new Examen(6L, "Inglés"),
+            new Examen(7L, "Historia"));
+
+    public final static List<String> PREGUNTAS = Arrays.asList("aritmética",
+            "integrales",
+            "derivadas",
+            "trigonometría",
+            "geometría");
+}
--- src/test/java/org/abundis/appmockito/ejemplos/services/ExamenServiceImplTest.java
+++ src/test/java/org/abundis/appmockito/ejemplos/services/ExamenServiceImplTest.java
@@ -29,11 +29,7 @@ class ExamenServiceImplTest {
 
     @Test
     void findExamenPorNombre() {
-        List<Examen> datos = Arrays.asList(new Examen(5L,"Matemáticas"),
-                new Examen(6L, "Inglés"),
-                new Examen(7L, "Historia"));
-
-        when(repository.findAll()).thenReturn(datos);
+        when(repository.findAll()).thenReturn(Datos.EXAMENES);
         Optional<Examen> examen = service.findExamenPorNombre("Matemáticas");
 
         assertTrue(examen.isPresent());
@@ -49,5 +45,17 @@ class ExamenServiceImplTest {
         Optional<Examen> examen = service.findExamenPorNombre("Matemáticas");
 
         assertFalse(examen.isPresent());
+    }
+
+    @Test
+    void testPreguntasExamen() {
+        when(repository.findAll()).thenReturn(Datos.EXAMENES);
+        when(preguntaRepository.findPreguntasPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS);
+
+        Examen examen = service.findExamenPorNombreConPreguntas("Matemáticas");
+
+        assertEquals(5, examen.getPreguntas().size());
+        assertTrue(examen.getPreguntas().contains("integrales"));
+
     }
 }
